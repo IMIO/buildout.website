76f17527b2c0bdfcab53f788d2f14ca4ebe4b365
Benoît Suttor <benoit.suttor@imio.be>
2014-11-25 12:52:32 +0100

    update automate migration

--- Makefile
+++ Makefile
@@ -50,8 +50,6 @@ mrbob: bin/python
 migration: bootstrap.py bin/python
 	ln -fs migration.cfg buildout.cfg
 	bin/buildout -t 7
-	bin/instance run migration.py
-	ln -fs prod.cfg buildout.cfg
+	bin/instance1 run migration.py
+	ln -fs dev.cfg buildout.cfg
 	make buildout
-	bin/upgrade-portals
-
--- migration.py
+++ migration.py
@@ -3,29 +3,45 @@ import logging
 from zope.globalrequest import setRequest
 from Testing import makerequest
 from Products.CMFCore.utils import getToolByName
+from AccessControl.SecurityManagement import newSecurityManager
+from zope.component.hooks import setSite
 logger = logging.getLogger('create.plonesite')
 
 
 def main(app):
+    acl_users = app.acl_users
+    user = acl_users.getUser('admin')
+    if user:
+        user = user.__of__(acl_users)
+        newSecurityManager(None, user)
+        logger.info("Retrieved the admin user")
     app = makerequest.makerequest(app)
     # support plone.subrequest
     app.REQUEST['PARENTS'] = [app]
     setRequest(app.REQUEST)
     container = app.unrestrictedTraverse('/')
     portal = get_plone_site(container)
-    setup_tool = getToolByName(portal, 'portal_setup')
-    setup_tool.runAllImportStepsFromProfile('profile-cpskin.migration:default')
-    setup_tool.runAllImportStepsFromProfile('profile-collective.directory:migration')
+    setSite(portal)
+    portal_setup = getToolByName(portal, 'portal_setup')
+    logger.info('---------- Start cpskin core profile ----------')
+    portal_setup.runAllImportStepsFromProfile('profile-cpskin.core:default')
+    logger.info('---------- Start cpskin MIGRATION profile ----------')
+    portal_setup.runAllImportStepsFromProfile('profile-cpskin.migration:default')
+    portal_setup.runAllImportStepsFromProfile('profile-collective.directory:migration')
+    portal_migration = getToolByName(portal, 'portal_migration')
+    portal_migration.upgrade()
 
 
 def get_plone_site(container):
+    result = ''
     for obj in container.values():
         if obj.meta_type == "Folder":
             for plone in obj.values():
                 if plone.meta_type == "Plone Site":
                     result = plone
-        elif obj.meta_type == "Plone Site":
-            result = obj
+        else:
+            if obj.meta_type == "Plone Site" and not result:
+                result = obj
     return result
 
 
